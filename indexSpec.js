5669eba46cd2f1423b426294ce5225521950b145
surya <ssureshbabu@tagworldwide.com>
2017-07-11 11:57:40 +0530

    Instantiated test cases

---
+++ indexSpec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+// var index = require "js/index.js";
+
+describe('data',function(){
+  // it('should be a json object',function(){
+  //   expect(index.data).to.have.property('members');
+  // })
+})
